e22cf343be9ed43b6105b41582961e06a54eb132
Katakam <katakam@Katakams-Laptop.local>
2025-06-13 01:09:47 -0400

    updated

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -7,27 +7,6 @@ resource "aws_ecs_cluster" "main" {
   }
 }
 
-# ECS Task Execution Role
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 # ECS Task Definition
 resource "aws_ecs_task_definition" "backend" {
   family                   = "ecs-demo-backend"
@@ -35,7 +14,7 @@ resource "aws_ecs_task_definition" "backend" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
       name      = "backend"
